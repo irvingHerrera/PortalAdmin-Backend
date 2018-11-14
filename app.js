e1dc522eb185f9c0eb94eb35465c521948c2c76e
Irving Herrera <irving.herreramolina@gmail.com>
2018-11-13 22:38:47 -0600

    se agrega mongoose al proyecto con mongo db

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // Requies
 var express = require('express');
+var mongoose = require('mongoose');
 
 // inicializar variables
 var app = express();
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/HospitalDB', (err, res) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log("Base de datos: online");
+});
+
+// Rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+});
 
 // Escuchar peticiones
 app.listen(3001, () => {
