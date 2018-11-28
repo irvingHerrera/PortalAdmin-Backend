b0cfbb1d74962c6922f2993bc99b429f421a6261
Irving Herrera <irving.herreramolina@gmail.com>
2018-11-28 11:09:04 -0600

    eliminar medico

--- routes/medico.js
+++ routes/medico.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var jwt = require('jsonwebtoken');
-var t = '';
+
 var mdAutenticacion = require('../middlewares/autenticacion');
 
 var app = express();
@@ -27,6 +27,55 @@ app.get('/', (req, res, next) => {
                     medicos: medicos
                 });
             });
+});
+
+// ==============================
+// Actualizar medico
+// ==============================
+
+app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Medico.findById(id, (err, medico) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar medico',
+                errors: err
+            });
+        }
+
+        if (!medico) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El medico con el id' + id + ' no existe',
+                errors: { message: 'No existe un medico con ese ID' }
+            });
+        }
+
+
+        medico.nombre = body.nombre;
+
+        medico.save((err, medicoGuardado) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar medico',
+                    errors: err
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                medico: medicoGuardado
+            });
+        });
+
+    });
+
 });
 
 // ==============================
@@ -57,6 +106,41 @@ app.post('/', mdAutenticacion.verificaToken, (req, res) => {
             usuarioToken: req.usuario
         });
     });
+});
+
+// ==============================
+// Borrar un medico por el id
+// ==============================
+
+app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
+
+    var id = req.params.id;
+
+    Medico.findByIdAndRemove(id, (err, medicoBorrado) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar medico',
+                errors: err
+            });
+        }
+
+        if (!medicoBorrado) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'No existe un medico con ese id',
+                errors: { message: 'No existe un medico con ese id' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            medico: medicoBorrado
+        });
+
+    });
+
 });
 
 module.exports = app;
