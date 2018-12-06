27f975b0d7ba9971714a752f77625a05e9fe43f9
Irving Herrera <irving.herreramolina@gmail.com>
2018-12-05 22:57:58 -0600

    login con google y creacion de usuario google

--- routes/login.js
+++ routes/login.js
@@ -41,19 +41,78 @@ app.post('/google', async(req, res) => {
 
     var token = req.body.token;
 
-    var googleUser = await verify(token)
-        .catch(e => {
-            return res.status(403).json({
-                ok: false,
-                mensaje: 'Token no valido'
-            });
+    try {
+        var googleUser = await verify(token)
+    } catch (err) {
+        res.status(403).json({
+            ok: false,
+            mensaje: 'Token de google no valido',
+            errors: { menssage: "Token de google no valido" }
         });
 
-    return res.status(200).json({
+        return;
+    }
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (usuarioDB) {
+
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe de usuar su autenticación normal',
+                    errors: err
+                });
+            } else {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB.id,
+                    mensaje: 'Login post correcto'
+                });
+            }
+        } else {
+            // El usuario no existe... hay que crearlo
+            var usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB.id,
+                    mensaje: 'Login post correcto'
+                });
+            });
+        }
+
+    });
+
+
+    /*return res.status(200).json({
         ok: true,
         mensaje: 'OK!!!',
         googleUser: googleUser
-    })
+    })*/
 });
 
 // ======================================
@@ -105,5 +164,6 @@ app.post('/', (req, res) => {
     });
 
 });
+
 
 module.exports = app;
